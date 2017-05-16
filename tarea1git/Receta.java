ec600634884c73279d6867c081602cca8883effa
Guillermo Espinosa <guillespinosa77@gmail.com>
2017-05-16 09:21:27 +0200

    Añadidos a la clase Receta 3 atributos de tipo String: nombre, ingredientes y preparacion. Constructores; uno solo con el nombre y otro con todos los campos. Getters y Setters.
    Signed-off-by: Guillermo Espinosa <guillespinosa77@gmail.com>

--- tarea1git/Receta.java
+++ tarea1git/Receta.java
@@ -2,4 +2,42 @@ package tarea1git;
 
 public class Receta {
 
+	private String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getIngredientes() {
+		return ingredientes;
+	}
+
+	public void setIngredientes(String ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	public String getPreparacion() {
+		return preparacion;
+	}
+
+	public void setPreparacion(String preparacion) {
+		this.preparacion = preparacion;
+	}
+
+	private String ingredientes;
+	private String preparacion;
+	
+	public Receta(String nombre){
+		this.nombre=nombre;
+	}
+	
+	public Receta(String nombre, String ingredientes, String preparacion){
+		this.nombre=nombre;
+		this.ingredientes=ingredientes;
+		this.preparacion=preparacion;
+	}
+	
 }
