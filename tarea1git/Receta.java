89552395ecff9e9b20c273387d4c68e1ec31bcf6
Guillermo Espinosa <guillespinosa77@gmail.com>
2017-05-17 13:10:35 +0200

    Sustitucion del atributo preparacion por un ArrayList de String.
    Signed-off-by: Guillermo Espinosa <guillespinosa77@gmail.com>

--- tarea1git/Receta.java
+++ tarea1git/Receta.java
@@ -1,5 +1,7 @@
 package tarea1git;
 
+import java.util.ArrayList;
+
 public class Receta {
 
 	private String nombre;
@@ -19,22 +21,22 @@ public class Receta {
 		this.ingredientes = ingredientes;
 	}
 
-	public String getPreparacion() {
+	public ArrayList<String> getPreparacion() {
 		return preparacion;
 	}
 
-	public void setPreparacion(String preparacion) {
+	public void setPreparacion(ArrayList<String> preparacion) {
 		this.preparacion = preparacion;
 	}
 
 	private String ingredientes;
-	private String preparacion;
+	private ArrayList<String> preparacion;
 	
 	public Receta(String nombre){
 		this.nombre=nombre;
 	}
 	
-	public Receta(String nombre, String ingredientes, String preparacion){
+	public Receta(String nombre, String ingredientes, ArrayList<String> preparacion){
 		this.nombre=nombre;
 		this.ingredientes=ingredientes;
 		this.preparacion=preparacion;
