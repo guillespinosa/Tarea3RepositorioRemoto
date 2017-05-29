f658ac54cc1c1c61048a74079b6935cfa72cf599
Guillermo Espinosa <guillespinosa77@gmail.com>
2017-05-29 08:55:18 +0200

    Desglosado el atributo de la clase Ingrediente en: float cantidad y String unidad.

--- tarea1git/Ingredientes.java
+++ tarea1git/Ingredientes.java
@@ -2,19 +2,29 @@ package tarea1git;
 
 public class Ingredientes {
 
-	private String cantidad;
+	private Float cantidad;
+	private String unidad;
+	
+	public String getUnidad() {
+		return unidad;
+	}
+
+	public void setUnidad(String unidad) {
+		this.unidad = unidad;
+	}
+
 	private String nombre;
 	
-	public Ingredientes(String cantidad, String nombre){
+	public Ingredientes(Float cantidad, String nombre){
 		this.cantidad=cantidad;
 		this.nombre=nombre;
 	}
 
-	public String getCantidad() {
+	public Float getCantidad() {
 		return cantidad;
 	}
 
-	public void setCantidad(String cantidad) {
+	public void setCantidad(Float cantidad) {
 		this.cantidad = cantidad;
 	}
 
